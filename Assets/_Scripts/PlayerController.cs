e0ab4433a482fc18df14a0a1eab54e3037868b2b
exitshadow <exertingshadows@gmail.com>
2022-11-21 13:31:59 +0100

    added bit of forgotten poem

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -7,9 +7,16 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Nuke _nuke;
+    [SerializeField] private Transform _rotationTarget;
+    [SerializeField] private float _speed = 20f;
 
     public void Nuke()
     {
         _nuke.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void Update()
+    {
+        transform.RotateAround(_rotationTarget.position, transform.right, _speed * Time.deltaTime);
     }
 }
