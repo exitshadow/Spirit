08c9d9f20f2fc43f0b568a964398c9289165f9ad
exitshadow <exertingshadows@gmail.com>
2022-11-24 00:28:44 +0100

    writing my own method :metal:

--- Assets/_Scripts/Generators/EarthGenerator.cs
+++ Assets/_Scripts/Generators/EarthGenerator.cs
@@ -2,53 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// loosely based on Sebastian Lague’s planet generator
 public class EarthGenerator : MonoBehaviour
 {
-    MeshData CreateFace(Vector3 direction, int resolution)
+    [SerializeField] private int _subdivisions = 1;
+    [SerializeField] private int _baseResolution = 2;
+
+    private List<MeshData> chunksList = new List<MeshData>();
+
+    // todo
+    // create a cube with 6 simple quads with settings to 1, 1
+    // subdivisions will create more mesh chunks
+    // baseResolution will increase the resolution inside that mesh
+
+    /// <summary>
+    /// Creates a list of MeshData chunks forming a square mesh facing a specified direction.
+    /// </summary>
+    private MeshData[] CreateFaceChunks(Vector3 facingDirection, int subDivs, int res, float size = 1f)
     {
-        // axis A shifts everyone right hand side
-        Vector3 axisA = new Vector3(direction.y, direction.z, direction.x);
-        Vector3 axisB = Vector3.Cross(direction, axisA);
+        int nbVerts = (res + 1) * (res + 1);    // number of vertices in a chunk
+        float chunkSize = size / subDivs;       // the width of each individual chunk
+        float chunkResStep = chunkSize / res;   // the distance between vertices in a chunk
 
-        Vector3[] verts = new Vector3[resolution * resolution];
-        int[] tris = new int[(resolution - 1) * (resolution - 1) * 6];
-        int triIndex = 0;
+        MeshData[] chunks = new MeshData[subDivs * subDivs];
 
-        for (int y = 0; y < resolution; y++)
+        // iterate in the number of chunks to make
+        for (int i = 0; i < subDivs; i++)
         {
-            for (int x = 0; x < resolution; x++)
-            {
-                int vertIndex = x + y * resolution;
-                Vector2 t = new Vector2(x, y) / (resolution - 1f);
-                Vector3 point = direction + axisA * (2 * t.x - 1) + axisB * (2 * t.y -1);
-                verts[vertIndex] = point;
+            int index = 0;
+            chunks[i].vertices = new Vector3[nbVerts];
+            chunks[i].triangles = new int[nbVerts];
 
-                if (x != resolution - 1 && y != resolution -1)
+            for (int x = 0; x < res; x++)
+            {
+                for (int y = 0; y < res; y++)
                 {
-                    tris[triIndex + 0] = vertIndex;
-                    tris[triIndex + 1] = vertIndex + resolution + 1;
-                    tris[triIndex + 2] = vertIndex + resolution;
-                    tris[triIndex + 3] = vertIndex;
-                    tris[triIndex + 4] = vertIndex + 1;
-                    tris[triIndex + 5] = vertIndex + resolution + 1;
-                    triIndex += 6;
+                    float distX = i * x + chunkResStep * x;
+                    float distY = i * y + chunkResStep * y;
+                    chunks[i].vertices[index] = new Vector3(distX, distY, size / 2);
                 }
             }
         }
 
-        return new MeshData(verts, tris);
+        return chunks;
     }
+}
 
-    private class MeshData
+public class MeshData
+{
+    public Vector3[] vertices;
+    public int[] triangles;
+
+    public MeshData(Vector3[] vertices, int[] triangles)
     {
-        Vector3[] vertices;
-        int[] triangles;
-
-        public MeshData(Vector3[] vertices, int[] triangles)
-        {
-            this.vertices = vertices;
-            this.triangles = triangles;
-        }
+        this.vertices = vertices;
+        this.triangles = triangles;
     }
 }
