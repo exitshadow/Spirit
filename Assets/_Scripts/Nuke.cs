c3ca1e37974d38d44d63554961bf0ca5c4519c28
exitshadow <exertingshadows@gmail.com>
2022-11-23 13:11:58 +0100

    fixed bugs, rm unnecessary dependency & refactored

--- Assets/_Scripts/Nuke.cs
+++ Assets/_Scripts/Nuke.cs
@@ -3,21 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Rigidbody))]
 public class Nuke : MonoBehaviour
 {
     [SerializeField] private Transform _earthTransform;
     [SerializeField] private float _earthSurfaceRadius = 6000;
     [SerializeField] private float _dropSpeed = 200f;
-    [SerializeField] private float _followFactor = 1f;
     public UnityEvent Boom;
 
-    private Vector3 launchPos;
-    private Vector3 currentPos;
-    private Vector3 deltaPos;
-
     private float startSpeed;
-
     private bool hasLaunched = false;
     private bool hasLanded = false;
 
@@ -28,7 +21,6 @@ public class Nuke : MonoBehaviour
         {
             Debug.Log("launching");
             hasLaunched = true;
-            launchPos = transform.parent.position;
             transform.SetParent(null);
             this.startSpeed = speed;
         }
@@ -36,22 +28,31 @@ public class Nuke : MonoBehaviour
 
     private void Update()
     {
+        if (hasLanded) return;
+
         if (hasLaunched)
-        {
-            currentPos = new Vector3(launchPos.x, launchPos.y, launchPos.z * startSpeed);
-            deltaPos = (launchPos - currentPos) * _followFactor;
+            FallTowardsEarth();
 
-            float step = _dropSpeed * Time.fixedDeltaTime;
+        CheckIfLanded();
+    }
 
-            transform.position = deltaPos + Vector3.MoveTowards(transform.position, _earthTransform.position, step);
-        }
+    private void FallTowardsEarth()
+    {
+        float step = _dropSpeed * Time.fixedDeltaTime;
 
+        Vector3 stepTowardsEarth = Vector3.MoveTowards(transform.position, _earthTransform.position, step);
+        Vector3 stepForward = new Vector3(0, 0, transform.position.z * startSpeed * Time.fixedDeltaTime);
+        transform.position = stepForward + stepTowardsEarth;
+    }
+
+    private void CheckIfLanded()
+    {
         float dist = Vector3.Distance(_earthTransform.position, transform.position);
         if (dist < _earthSurfaceRadius && !hasLanded)
         {
-            hasLanded = true;
             Debug.Log("Boom");
             Boom?.Invoke();
+            hasLanded = true;
         }
     }
 }
