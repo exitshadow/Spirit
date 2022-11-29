668a807fd54ae704672339418750c9e07a7a69a6
exitshadow <exertingshadows@gmail.com>
2022-11-29 15:36:41 +0100

    implemented text fade in out w animated curves :ok_hand:

--- Assets/_Scripts/Poem Display/AnimationController.cs
+++ Assets/_Scripts/Poem Display/AnimationController.cs
@@ -9,9 +9,20 @@ public class AnimationController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _TMPFieldToFill;
     [SerializeField] private float _timeToWaitBeforeStart;
     [SerializeField] private float _fadingTimeIn;
+    [SerializeField] private AnimationCurve _fadingCurveIn;
     [SerializeField] private float _fadingTimeOut;
+    [SerializeField] private AnimationCurve _fadingCurveOut;
+
     private List<PoemSnippet> _snippets;
-    private bool _hasStarted = false;
+
+    private float fadeInStart;
+    private float fadeInEnd;
+    private float fadeOutStart;
+    private float fadeOutEnd;
+
+    private bool hasStarted = false;
+    private bool isFadingOut = false;
+    private bool isFadingIn = false;
 
     private void Awake()
     {
@@ -27,10 +38,10 @@ public class AnimationController : MonoBehaviour
     IEnumerator DisplaySnippet()
     {
         // waits for time
-        if (!_hasStarted)
+        if (!hasStarted)
         {
             yield return new WaitForSeconds(_timeToWaitBeforeStart);
-            _hasStarted = true;
+            hasStarted = true;
         }
 
         for (int i = 0; i < _snippets.Count; i++)
@@ -38,8 +49,42 @@ public class AnimationController : MonoBehaviour
             // shows first snippet
             _TMPFieldToFill.text = _snippets[i].Content;
 
+            // fade in
+            if (!isFadingIn)
+            {
+                fadeInStart = Time.time;
+                fadeInEnd = fadeInStart + _fadingTimeIn;
+                isFadingIn = true;
+            }
+
+            while (isFadingIn)
+            {
+                float t = MathUtils.InverseLerp(fadeInStart, fadeInEnd, Time.time);
+                _TMPFieldToFill.alpha = _fadingCurveIn.Evaluate(t);
+                if (Time.time > fadeInEnd) isFadingIn = false;
+                yield return null;
+            }
+
+
             // waits its display time
             yield return new WaitForSeconds(_snippets[i].DisplayTime);
+
+
+            // fade out
+            if (!isFadingOut)
+            {
+                fadeOutStart = Time.time;
+                fadeOutEnd = fadeOutStart + _fadingTimeOut;
+                isFadingOut = true;
+            }
+
+            while (isFadingOut)
+            {
+                float t = MathUtils.InverseLerp(fadeOutStart, fadeOutEnd, Time.time);
+                _TMPFieldToFill.alpha = _fadingCurveOut.Evaluate(t);
+                if (Time.time > fadeOutEnd) isFadingOut = false;
+                yield return null;
+            }
 
             // masks it
             _TMPFieldToFill.text = "";
