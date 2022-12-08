2d5a82dd65fa25bdc2e1c28165516cfd23582d9a
exitshadow <exertingshadows@gmail.com>
2022-12-08 14:01:08 +0100

    this is fun

--- Assets/_Scripts/Ditch/TerrainGenerator.cs
+++ Assets/_Scripts/Ditch/TerrainGenerator.cs
@@ -6,23 +6,63 @@ public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private GameObject _previewPrefab;
+    [SerializeField] private Mesh _sliceMesh;
     [SerializeField] private int _maxDistX;
     [SerializeField] private int _maxDistZ;
-    [SerializeField] private int _maximumHeight;
-    [SerializeField] private int _maximumDepth;
     [SerializeField] private int _resolution;
     [SerializeField] [Range(0, 10f)] private float _horizontalIntensity = 2f;
     [SerializeField] [Range(0, 10f)] private float _verticalIntensity = 2f;
+    private float step;
 
     private Hashtable chunksContainer = new Hashtable();
     private List<Vector3> chunksPositions = new List<Vector3>();
-    private float step;
-    private float stepY;
 
     private void Start()
     {
         step = _maxDistX / _resolution;
-        GeneratePositions();
+
+        if (_sliceMesh == null)
+        {
+            GeneratePositions();
+        }
+        else
+        {
+            CastVertsPositions(_sliceMesh);
+        }
+        
+    }
+
+    private void CastVertsPositions(Mesh mesh)
+    {
+        Vector3[] verts = new Vector3[mesh.vertices.Length];
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(mesh.vertices[i]);
+
+            float posX = worldPos.x
+                         + (float)NoiseS3D.Noise(worldPos.x, worldPos.z)
+                         * _horizontalIntensity;
+            float posZ = worldPos.z
+                         + (float)NoiseS3D.Noise(worldPos.x, worldPos.z)
+                         * _horizontalIntensity;
+            float posY = (float)NoiseS3D.Noise(posX, posZ) * _verticalIntensity;
+
+            worldPos = new Vector3(posX, posY, posZ);
+
+            Vector3 newLocalPos = transform.InverseTransformPoint(worldPos);
+
+            verts[i] = newLocalPos;
+        }
+
+        var meshFilter = GetComponent<MeshFilter>();
+        Mesh newMesh = new Mesh();
+        newMesh.name = "Modified Terrain";
+        newMesh.vertices = verts;
+        newMesh.triangles = mesh.triangles;
+        newMesh.RecalculateNormals();
+
+        meshFilter.sharedMesh = newMesh;
     }
 
     private void GeneratePositions()
